9b841975f172bdd9eaea666caed96a08de59e53c
James K. <jkurtz@tenth.org>
2024-08-09 16:05:18 -0400

    Version bump

--- DigSig.php
+++ DigSig.php
@@ -14,7 +14,7 @@ Plugin Name:        DigSig
 Plugin URI:         https://github.com/tenthpres/digsig
 GitHub Plugin URI:  https://github.com/tenthpres/digsig
 Description:        A WordPress Plugin for Digital Signage
-Version:            1.0.5
+Version:            1.0.6
 Author:             James K
 Author URI:         https://github.com/jkrrv
 License:            MIT
--- src/DigSig/DigSig.php
+++ src/DigSig/DigSig.php
@@ -21,7 +21,7 @@ class DigSig
     /**
      * Version number
      */
-    public const VERSION = "1.0.5";
+    public const VERSION = "1.0.6";
 
     public const DEBUG = false;
 
